77815bb9bef847f5f413036fa9225c5f6b3e2793
Michael Norman <mjnorms@gmail.com>
2024-04-30 18:00:14 -0700

    shuffled assets into their own folder

---
+++ Source/Frontier/Weapon/Weapon.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class FRONTIER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+public:	
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+
+};
