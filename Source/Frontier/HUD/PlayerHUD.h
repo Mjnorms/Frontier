9695822bbb6527115abc189ddf60bbcb941de9d0
Michael Norman <mjnorms@gmail.com>
2025-01-27 19:37:51 -0800

    playerHUD and PlayerController

---
+++ Source/Frontier/HUD/PlayerHUD.h
@@ -0,0 +1,17 @@
+// Mjnorms -- 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "PlayerHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTIER_API APlayerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Frontier/PlayerController/FrontierPlayerController.h
@@ -0,0 +1,17 @@
+// Mjnorms -- 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FrontierPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTIER_API AFrontierPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
